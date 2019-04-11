d213da63d393feeb8903cf14f7e888404cc71f40
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-04-11 10:54:01 +0100

    Initila commit

---
+++ src/main/java/com/example/codeclan/Booking/System/models/Booking.java
@@ -0,0 +1,6 @@
+package com.example.codeclan.Booking.System.models;
+
+public class Booking {
+
+    public
+}
---
+++ src/main/java/com/example/codeclan/Booking/System/models/Course.java
@@ -0,0 +1,55 @@
+package com.example.codeclan.Booking.System.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+
+    private String name;
+    private String town;
+    private int rating;
+    private List<Booking> bookings;
+
+
+    public Course() {
+
+    }
+    public Course(String name, String town, int rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+}
